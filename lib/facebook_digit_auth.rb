ed07599bd1038ce71a2848dcffd1d56b447b5d12
RaghavSingh <Raghav.Singh@lptpl.com>
2016-12-07 19:08:48 +0530

    update version

--- lib/facebook_digit_auth.rb
+++ lib/facebook_digit_auth.rb
@@ -1,12 +1,13 @@
 require "facebook_digit_auth/version"
 module FacebookDigitAuth 
-
-	def self.test_method(params,type)
-		if type == 'facebook'
-			FacebookClient.new(params).data
-		elsif type == 'digit'
-			DigitsClient.new(params).data
+ 	class Fd
+		def self.test_method(params,type)
+			if type == 'facebook'
+				FacebookClient.new(params).data
+			elsif type == 'digit'
+				DigitsClient.new(params).data
+			end
 		end
-	end
+	end	
 
 end
